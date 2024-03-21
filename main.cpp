b83cae364b3f453ce29bdb3d3b64cdc837a470dd
tanao <tanao030801@foxmail.com>
2024-03-22 00:31:35 +0800

    modified CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -30,10 +30,8 @@ void Init()
         int id;
         scanf("%d",&id);
         scanf("%d%d%d%d",&berth[id].y,&berth[id].x,&berth[id].transport_time,&berth[id].loading_speed);
-        // cerr<<berth[id].transport_time<<" ";
-        // cerr<<berth[id].loading_speed<<" ";
     }
-    scanf("%d",&boat_capacity); // cerr<<boat_capacity<<endl;
+    scanf("%d",&boat_capacity);
     char okk[100];
     scanf("%s",okk);
     printf("OK\n");
@@ -59,7 +57,6 @@ void Input()
     {
         int x,y,v;
         scanf("%d%d%d",&y,&x,&v);
-        // if(v<100)continue;
         exist_gds[y][x]={x,y,t0,v,0};
     }
     for(int i=0;i<robot_num;i++)
@@ -67,7 +64,6 @@ void Input()
         robot[i].id=i,robot[i].t0=t0;
         scanf("%d%d%d%d",&robot[i].gds,&robot[i].y,&robot[i].x,&robot[i].status);
         robot_data[i].x=robot[i].x,robot_data[i].y=robot[i].y,robot_data[i].pri=robot[i].get_priority();
-        // if(robot[i].status==0)while(1)cerr<<"collision!"<<endl;
     }
     for(int i=0;i<boat_num;i++)
     {
