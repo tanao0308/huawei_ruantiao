120200fe5b9ffc0c5ec2720e90bf767d6f4c4838
tanao <tanao030801@foxmail.com>
2024-03-21 20:29:23 +0800

    modified test.py:3

--- testcode/test.py
+++ testcode/test.py
@@ -9,7 +9,7 @@ seed_num = 3
 def work(i, j):
     PreliminaryJudge_path = "D:/Desktop/huawei_ruantiao/WindowsReleasev1.2/PreliminaryJudge"
     map_command = " -m D:/Desktop/huawei_ruantiao/WindowsReleasev1.2/maps/map{}.txt".format(i + 1)
-    seed_command = " -s {}".format(j)
+    seed_command = " -s {}".format(j + 1)
     main_path = " \"D:/Desktop/huawei_ruantiao/build/main.exe\""
     command_to_execute = PreliminaryJudge_path + map_command + seed_command + main_path
     # print(command_to_execute)
